1e8102bb817c90d0351b53e1bf2edbc53197c834
Maksym Dubovenko <Maksym Dubovenko>
2018-02-25 18:13:36 +0200

    added getPaper method

--- classes/Research.php
+++ classes/Research.php
@@ -17,13 +17,32 @@ class Research {
 		$this->data = $data;
 	}
 	
-		public static function GetResearch(): ?Research {
+	public static function GetResearch(): ?Research {
 		$query = "	SELECT research_id, paper_id, paper_name FROM papers
 					WHERE online = :online";
 
 		$researchData = self::Database()
 					->select($query)
 					->binds(':online', 1)
+					->execute()
+					->fetch_all();
+
+		if (empty($researchData)) {
+			return null;
+		}
+
+		return new self($researchData);
+	}
+	
+	public static function GetPaper(int $paper_id): ?Research {
+		$query = "	SELECT q.question_id, q.question, v.variant_id, v.variant_text 
+					FROM `questions` q
+					LEFT JOIN variants v ON q.question_id = v.question_id 
+					WHERE q.paper_id = :paper_id";
+
+		$researchData = self::Database()
+					->select($query)
+					->binds(':paper_id', $paper_id)
 					->execute()
 					->fetch_all();
 
