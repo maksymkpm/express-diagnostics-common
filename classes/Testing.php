26498f01c6105ccdce26ba07bf7f5f2927246f4e
Maksym Dubovenko <Maksym Dubovenko>
2018-03-04 18:06:02 +0200

    updated return results with config for summary

--- classes/Testing.php
+++ classes/Testing.php
@@ -29,54 +29,64 @@ class Testing {
 		return self::returnResults($score * 100);
 	}
 	
+	public static function scoreForSlider($score) {
+		return int ($score * 100);
+	}
+	
 	public static function returnResults($score) {
-		return '<table width="100%" cellpadding="0" cellspacing="0">	
-			<tr>		
-				<td style="vertical-align: top; padding: 10px">
-					<span style="font-weight: bold; color: #181260">Индекс здоровья координирующего комплекса: .</span>
-					<br/><span style="color: #ff8900; font-weight: bold">Заключение: </span>
-				</td>
-			</tr>	
-			<tr>
-				<td style="padding-bottom: 20px">
-					<form method="get" name="demoForm">
-						<input name="sliderValue" id="score" type="hidden" size="3" value="' . $score . '">
-							<script language="JavaScript">
-								var A_TPL = {
-									"b_vertical" : false,
-									"b_watch": true,
-									"n_controlWidt": 555,
-									"n_controlHeight": 16,
-									"n_sliderWidth": 5,
-									"n_sliderHeight": 25,
-									"n_pathLeft" : 1,
-									"n_pathTop" : 1,
-									"n_pathLength" : 550,
-									"s_imgControl": "images/blueh_bg.gif",
-									"s_imgSlider": "images/blueh_sl.gif",
-									"n_zIndex": 1
-								}
+		$summary = '';
+		
+		//заключение по координирующему комплексу
+		if ($score == 0) {
+			$summary = config::get('recommendation.1.perfect');
+		}
+		elseif (($score > 0) && ($score <= 0.33)) {
+			$summary = config::get('recommendation.1.good');
+		}
+		elseif (($score > 0.33) && ($score <= 0.66)) {
+			$summary = config::get('recommendation.1.middle');
+		}
+		elseif (($score > 0.66) && ($score <= 1)) {
+			$summary = config::get('recommendation.1.bad');
+		}
+		
+		return '
+		<div>Индекс здоровья координирующего комплекса: ' . $score . '. </div>
+		<div>Заключение: ' . $summary . '</div>
+		<form method="get" name="demoForm">
+			<input name="sliderValue" id="score" type="hidden" size="3" value="' . self::scoreForSlider($score) . '">
+				<script language="JavaScript">
+					var A_TPL = {
+						"b_vertical" : false,
+						"b_watch": true,
+						"n_controlWidt": 555,
+						"n_controlHeight": 16,
+						"n_sliderWidth": 5,
+						"n_sliderHeight": 25,
+						"n_pathLeft" : 1,
+						"n_pathTop" : 1,
+						"n_pathLength" : 550,
+						"s_imgControl": "images/blueh_bg.gif",
+						"s_imgSlider": "images/blueh_sl.gif",
+						"n_zIndex": 1
+					}
 
-								var A_INIT1 = {
-									"s_form" : 0,
-									"s_name": "score",
-									"n_minValue" : 0,
-									"n_maxValue" : 100,
-									"n_value" : 0,
-									"n_step" : 1
-								}
-								new slider(A_INIT1, A_TPL);
-							</script>
-						</form>
-				</td>
-			</tr>	
-			<tr>		
-				<td style="vertical-align: top; text-align: center; padding: 10px">			
-					<a href="index.php?page=research"><img src="images/another_tests.jpg" width="140px" border="0" alt="" /></a>&nbsp;&nbsp;&nbsp;
-					<a href="index.php?page=resume"><img src="images/to_reports.jpg" width="140px" border="0" alt="" /></a>&nbsp;&nbsp;&nbsp;	
-					<a href="index.php?page=results"><img src="images/to_results.jpg" width="140px" border="0" alt="" /></a>	
-				</td>
-			</tr>
-		</table>';
+					var A_INIT1 = {
+						"s_form" : 0,
+						"s_name": "score",
+						"n_minValue" : 0,
+						"n_maxValue" : 100,
+						"n_value" : 0,
+						"n_step" : 1
+					}
+					new slider(A_INIT1, A_TPL);
+				</script>
+			</form>
+			<div>		
+				<a href="index.php?page=research"><img src="images/another_tests.jpg" width="140px" border="0" alt="" /></a>&nbsp;&nbsp;&nbsp;
+				<a href="index.php?page=resume"><img src="images/to_reports.jpg" width="140px" border="0" alt="" /></a>&nbsp;&nbsp;&nbsp;	
+				<a href="index.php?page=results"><img src="images/to_results.jpg" width="140px" border="0" alt="" /></a>	
+			</div>';
 	}
 }
+s
