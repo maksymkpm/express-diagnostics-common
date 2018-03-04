c20427be8e63c955b3b0b675677f23d8cd990a5d
Maksym Dubovenko <Maksym Dubovenko>
2018-03-04 16:03:11 +0200

    updated paper1

--- classes/Testing.php
+++ classes/Testing.php
@@ -4,14 +4,16 @@ class Testing {
 	public static function Database(): \db {
 		$db = \db::connect('default');
 		$db->query('SET NAMES utf8');
-		
+
 		return $db;
 	}
-	
+
 	public static function Paper1(string $answers) {
 		$query = "	SELECT SUM(weight) AS score
 					FROM variants
 					WHERE variant_id IN (:answers)";
+
+		$answers = substr($answers, 0, -1);
 
 		$scoreData = self::Database()
 					->select($query)
@@ -19,8 +21,12 @@ class Testing {
 					->execute()
 					->fetch();
 		
-		$score = (16 - $scoreData['score']) / 16;
-		
+		if ($scoreData['score'] > 16) {
+			$score = 0;
+		} else {
+			$score = (16 - $scoreData['score']) / 16;
+		}
+
 		return $score;
 	}
 }
