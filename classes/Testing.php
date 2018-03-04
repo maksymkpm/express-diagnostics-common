44bc28966afe4eb2e96ab1b50f1dd6e32ad2ddc7
Maksym Dubovenko <Maksym Dubovenko>
2018-03-04 15:49:59 +0200

    updated testing class with calculating score of paper 1

--- classes/Testing.php
+++ classes/Testing.php
@@ -1,1 +1,26 @@
 <?php
+
+class Testing {
+	public static function Database(): \db {
+		$db = \db::connect('default');
+		$db->query('SET NAMES utf8');
+		
+		return $db;
+	}
+	
+	public static function Paper1(array $answers) {
+		$query = "	SELECT SUM(weight) AS score
+					FROM variants
+					WHERE variant_id IN (:answers)";
+
+		$scoreData = self::Database()
+					->select($query)
+					->binds(':answers', $answers)
+					->execute()
+					->fetch();
+		
+		$score = (16 - $scoreData['score']) / 16;
+		
+		return $score;
+	}
+}
