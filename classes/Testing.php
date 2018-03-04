21e453b17cedb32bcdb6b95cd1bd4a7c141f1941
Maksym Dubovenko <Maksym Dubovenko>
2018-03-04 18:17:11 +0200

    updated return resuts

--- classes/Testing.php
+++ classes/Testing.php
@@ -26,7 +26,7 @@ class Testing {
 			$score = (16 - $scoreData['score']) / 16;
 		}
 
-		return self::returnResults($score * 100);
+		return self::returnResults($score);
 	}
 	
 	public static function scoreForSlider($score) {
@@ -39,53 +39,35 @@ class Testing {
 		//заключение по координирующему комплексу
 		if ($score == 0) {
 			$summary = config::get('recommendation.1.perfect');
+			$settings = ["good" => 1, "middle" => 0.3, "bad" => 0.3];
 		}
 		elseif (($score > 0) && ($score <= 0.33)) {
 			$summary = config::get('recommendation.1.good');
+			$settings = ["good" => 1, "middle" => 0.5, "bad" => 0.3];
 		}
 		elseif (($score > 0.33) && ($score <= 0.66)) {
 			$summary = config::get('recommendation.1.middle');
+			$settings = ["good" => 0.5, "middle" => 1, "bad" => 0.5];
 		}
 		elseif (($score > 0.66) && ($score <= 1)) {
 			$summary = config::get('recommendation.1.bad');
+			$settings = ["good" => 0.3, "middle" => 0.5, "bad" => 1];
 		}
 		
 		return '
 		<div>Индекс здоровья координирующего комплекса: ' . $score . '. </div>
 		<div>Заключение: ' . $summary . '</div>
-		<form method="get" name="demoForm">
-			<input name="sliderValue" id="score" type="hidden" size="3" value="' . self::scoreForSlider($score) . '">
-				<script language="JavaScript">
-					var A_TPL = {
-						"b_vertical" : false,
-						"b_watch": true,
-						"n_controlWidt": 555,
-						"n_controlHeight": 16,
-						"n_sliderWidth": 5,
-						"n_sliderHeight": 25,
-						"n_pathLeft" : 1,
-						"n_pathTop" : 1,
-						"n_pathLength" : 550,
-						"s_imgControl": "images/blueh_bg.gif",
-						"s_imgSlider": "images/blueh_sl.gif",
-						"n_zIndex": 1
-					}
-
-					var A_INIT1 = {
-						"s_form" : 0,
-						"s_name": "score",
-						"n_minValue" : 0,
-						"n_maxValue" : 100,
-						"n_value" : 0,
-						"n_step" : 1
-					}
-					new slider(A_INIT1, A_TPL);
-				</script>
-			</form>
-			<div>		
-				<a href="index.php?page=research"><img src="images/another_tests.jpg" width="140px" border="0" alt="" /></a>&nbsp;&nbsp;&nbsp;
-				<a href="index.php?page=resume"><img src="images/to_reports.jpg" width="140px" border="0" alt="" /></a>&nbsp;&nbsp;&nbsp;	
-				<a href="index.php?page=results"><img src="images/to_results.jpg" width="140px" border="0" alt="" /></a>	
-			</div>';
+		<table width="100%">
+			<tr>
+				<td width="33%" style="color: greed; height: 30px; opacity: ' . $settings["good"] . '"></td>
+				<td width="33%" style="color: yellow; height: 30px; opacity: ' . $settings["middle"] . '"></td>
+				<td width="33%" style="color: red; height: 30px; opacity: ' . $settings["bad"] . '"></td>
+			</tr>
+		</table>
+		<div>		
+			<a href="index.php?page=research"><img src="images/another_tests.jpg" width="140px" border="0" alt="" /></a>&nbsp;&nbsp;&nbsp;
+			<a href="index.php?page=resume"><img src="images/to_reports.jpg" width="140px" border="0" alt="" /></a>&nbsp;&nbsp;&nbsp;	
+			<a href="index.php?page=results"><img src="images/to_results.jpg" width="140px" border="0" alt="" /></a>	
+		</div>';
 	}
 }
